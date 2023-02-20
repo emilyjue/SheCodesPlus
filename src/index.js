0aafe1841f20dc715e76834c6cf0c316d2b97e44
emilyjue <emilyjue@email.arizona.edu>
2023-02-20 10:24:28 -0600

    Adjust css and incorporate the SheCodes weather API

--- src/index.js
+++ src/index.js
@@ -1,83 +1,5 @@
-/*let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
-let city = prompt("Enter a city");
-let known = false;
-if (!city) {
-  alert("Please enter a city");
-} else {
-  city = city.toLowerCase();
-  for (let location in weather) {
-    if (city === location) {
-      let far = Math.round((weather[location].temp * 9) / 5 + 32);
-      let cap = city.charAt(0).toUpperCase() + city.slice(1);
-      alert(
-        `It is currently ${Math.round(
-          weather[location].temp
-        )}°C (${far}°F) in ${cap} with a humidity of ${
-          weather[location].humidity
-        }%`
-      );
-      known = true;
-      break;
-    }
-  }
-  if (known === false) {
-    alert(
-      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-    );
-  }
-}
-
-
-
-
-<div class="form-check form-check-inline" id="temp-unit">
-        <input
-          class="form-check-input"
-          type="radio"
-          name="flexRadioDefault"
-          id="fahrenheit"
-          checked
-        />
-        <label class="form-check-label" for="flexRadioDefault1" id="fahrenheit"
-          >Fahrenheit</label
-        >
-      </div>
-      <div class="form-check form-check-inline">
-        <input
-          class="form-check-input"
-          type="radio"
-          name="flexRadioDefault"
-          id="celsius"
-        />
-        <label class="form-check-label" for="flexRadioDefault2" id="celsius"
-          >Celsius</label
-        >
-      </div>
-    </div>
-*/
-let oldTemp = 23;
+let oldTemp = 0;
+let oldWind = 0;
 let now = new Date();
 let day = [
   "Sunday",
@@ -97,17 +19,28 @@ function search(event) {
   let newCity = document.querySelector("h1");
   let cap = input.value.charAt(0).toUpperCase() + input.value.slice(1);
   newCity.innerHTML = cap;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cap}&units=imperial&appid=${apiKey}`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cap}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(searchTemp);
 }
 
 function searchTemp(response) {
   let tempElement = document.querySelector("h2");
-  console.log(response.data.main.temp); //Challenge 4
-  let roundTemp = Math.round(response.data.main.temp);
+  let roundTemp = Math.round(response.data.temperature.current);
   tempElement.innerHTML = `${roundTemp}°F`;
   oldTemp = roundTemp;
+  let descElement = document.querySelector("#weather-description");
+  let windElement = document.querySelector("#wind-speed");
+  let wind = Math.round(response.data.wind.speed);
+  let description = response.data.condition.description;
+  description = description.charAt(0).toUpperCase() + description.slice(1);
+  console.log(response.data.condition.description);
+  let iconElement = document.querySelector("#weather-icon");
+  let icon = response.data.condition.icon_url;
+  console.log(icon);
+  iconElement.innerHTML = `<image src=${icon} style="width=50px;height=50px">`;
+  descElement.innerHTML = description;
+  windElement.innerHTML = `Windspeed: ${wind}mph`;
+  oldWind = wind;
 }
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", search);
@@ -132,35 +65,56 @@ function switchUnit(event) {
   }
   if (output === "fahrenheit") {
     let newTemp = Math.round((oldTemp * 9) / 5 + 32);
+    let newWind = Math.round(oldWind * 2.236936);
+    oldWind = newWind;
     oldTemp = newTemp;
     let newTempUnit = document.querySelector("h2");
     newTempUnit.innerHTML = newTemp + "&#176;F";
+    let newWindUnit = document.querySelector("#wind-speed");
+    newWindUnit.innerHTML = "Windspeed: " + newWind + "mph";
   } else {
     let newTemp = Math.round(((oldTemp - 32) * 5) / 9);
+    let newWind = Math.round(oldWind * 0.44704);
+    oldWind = newWind;
     oldTemp = newTemp;
     let newTempUnit = document.querySelector("h2");
     newTempUnit.innerHTML = newTemp + "&#176;C";
+    let newWindUnit = document.querySelector("#wind-speed");
+    newWindUnit.innerHTML = "Windspeed: " + newWind + "m/s";
   }
 }
 let tempUnit = document.querySelector("#temp-form");
 tempUnit.addEventListener("change", switchUnit);
 
-let apiKey = "794204046345249d3375e2b3e50ea702";
+let apiKey = "aa64f245c9c8be5o955890a153t25adb";
+
 function handlePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(currentTemp);
 }
 
 function currentTemp(response) {
   let tempElement = document.querySelector("h1");
   let tempFar = document.querySelector("h2");
-  console.log(response.data.main);
-  let roundTemp = Math.round(response.data.main.temp);
-  tempElement.innerHTML = `It is currently`;
-  tempFar.innerHTML = `${roundTemp}°F in ${response.data.name}`;
+  console.log(response.data);
+  let roundTemp = Math.round(response.data.temperature.current);
+  tempElement.innerHTML = response.data.city;
+  tempFar.innerHTML = `${roundTemp}°F`;
   oldTemp = roundTemp;
+  let descElement = document.querySelector("#weather-description");
+  let windElement = document.querySelector("#wind-speed");
+  let wind = Math.round(response.data.wind.speed);
+  let description = response.data.condition.description;
+  description = description.charAt(0).toUpperCase() + description.slice(1);
+  let iconElement = document.querySelector("#weather-icon");
+  let icon = response.data.condition.icon_url;
+  console.log(icon);
+  iconElement.innerHTML = `<image src=${icon} style="width=50px;height=50px;">`;
+  descElement.innerHTML = description;
+  windElement.innerHTML = `Windspeed: ${wind}mph`;
+  oldWind = wind;
 }
 
 function currentCity(event) {
@@ -170,3 +124,5 @@ function currentCity(event) {
 
 let cform = document.querySelector("#current-form");
 cform.addEventListener("submit", currentCity);
+
+navigator.geolocation.getCurrentPosition(handlePosition);
